51e4300d40c99d36a853e43ecc51e7774a8b36e2
reneromain <rene.romain@mail.com>
2017-11-07 17:50:07 +0100

    Throwback idea

--- js/game.js
+++ js/game.js
@@ -1,6 +1,9 @@
 var Game = {};
 var player;
 var idle, right, left, up, down;
+var enemies;
+var throwback = [];
+var button1, button2;
 // for web
 var easystar = new EasyStar.js();
 
@@ -14,6 +17,12 @@ Game.preload = function() {
     game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
     //game.load.image('sprite','assets/sprites/sprite.png');
     game.load.spritesheet('sprite', 'assets/sprites/spritesheet.png', 48, 48);
+    game.load.spritesheet('enemy', 'assets/sprites/spritesheet-enemy.png', 48, 48);
+    game.load.image('home', 'assets/sprites/home.png');
+
+    game.load.spritesheet('button1', 'assets/ui/throwback.png', 47, 45);
+    game.load.spritesheet('button2', 'assets/ui/play.png', 47, 45);
+
 };
 
 Game.create = function(){
@@ -27,16 +36,51 @@ Game.create = function(){
     var layer;
     for(var i = 0; i < map.layers.length; i++) {
         layer = map.createLayer(i);
-    }
-    Game.addPlayer(100, 100);
+    };
+
     layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
     layer.events.onInputUp.add(Game.getCoordinates, this);
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    Game.addPlayer(100, 100);
     idle = player.animations.add('idle', [0], 10, true);
     right = player.animations.add('right', [7, 8, 9, 10, 11, 12], 10, true);
     left = player.animations.add('left', [15, 16, 17, 18, 19, 20], 10, true);
     up = player.animations.add('up', [21, 22], 10, true);
+
+    //  Here we create 2 new groups
+    enemies = game.add.group();
+
+    for (var i = 0; i < 10; i++)
+    {
+        //  This creates a new Phaser.Sprite instance within the group
+        //  It will be randomly placed within the world and use the 'baddie' image to display
+        Game.addEnemies(Math.random() * 1024, Math.random() * 768);
+    };
+
+    home = game.add.sprite(1024 - 217, 768 - 244, 'home');
+    game.physics.arcade.enable(home);
+
+//ui
+    button1 = game.add.button(0, 0, 'button1', Game.actionOnClick1, this, 1, 0);
+    button2 = game.add.button(50, 0, 'button2', Game.actionOnClick2, this, 1, 0);
+    button1.fixedToCamera = true;
+    button2.fixedToCamera = true;
 };
+
+Game.update = function(){
+  game.physics.arcade.overlap(player, enemies, Game.collisionHandler, null, this);
+  game.physics.arcade.overlap(player, home, Game.collisionHome, null, this);
+};
+
+Game.collisionHandler = function(){
+  console.log("collide");
+};
+
+Game.collisionHome = function(){
+  console.log("you win");
+}
 
 Game.getCoordinates = function(layer, pointer){
     Game.movePlayer(pointer.worldX,pointer.worldY);
@@ -45,9 +89,17 @@ Game.getCoordinates = function(layer, pointer){
 Game.addPlayer = function(x, y){
     player = game.add.sprite(x, y, 'sprite', 0);
     player.smoothed = false;
+    game.physics.arcade.enable(player);
 
     game.camera.follow(player);
 };
+
+Game.addEnemies = function(x, y){
+  enemy = game.add.sprite(x, y, 'enemy', 0);
+  enemy.smoothed = false;
+  game.physics.arcade.enable(enemy);
+  enemies.add(enemy);
+}
 
 Game.movePlayer = function(x, y){
   var m = game.cache.getTilemapData('map').data.layers[0].data;
@@ -72,6 +124,8 @@ Game.movePlayer = function(x, y){
     	 console.log("Path was not found.");
     	} else {
     	 console.log("Path was found.");
+
+       throwback.push(path);
 
         if(this.ismoving === true){
           this.tween.stop();
@@ -115,3 +169,11 @@ Game.movePlayer = function(x, y){
     easystar.calculate();
 
 };
+
+Game.actionOnClick1 = function(){
+
+}
+
+Game.actionOnClick2 = function(){
+
+}
